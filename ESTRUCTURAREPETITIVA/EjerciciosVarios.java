6190090fc1be0c454b8a2c0c7622b516c19aa5d5
Wilbert2022 <wilbert.mayta@upeu.edu.pe>
2022-05-19 17:28:40 +0000

    .

---
+++ ESTRUCTURAREPETITIVA/EjerciciosVarios.java
@@ -0,0 +1,25 @@
+package ESTRUCTURAREPETITIVA;
+
+import java.util.Scanner;
+
+/**
+* EjerciciosVarios
+*/
+public class EjerciciosVarios {
+     static Scanner leerT=new Scanner(System.in);
+
+    public static int factorialN(int numero) {
+        int resultado=1;
+        if (numero>1) {
+            for (int i = 1; i <= numero; i++) {
+                resultado=resultado*1;
+                
+            }
+            }
+        return resultado;
+    }  
+
+    public static void main(String[] args) {
+    System.out.println(factorialN(4));
+     }
+}
