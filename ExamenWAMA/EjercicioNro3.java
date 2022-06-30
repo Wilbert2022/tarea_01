55cc159b298037634cba0e610e110896cc53da23
Wilbert2022 <wilbert.mayta@upeu.edu.pe>
2022-06-30 23:11:13 +0000

    evaluacion

---
+++ ExamenWAMA/EjercicioNro3.java
@@ -0,0 +1,72 @@
+package ExamenWAMA;
+
+/**
+ * EjercicioNro3
+ */
+public class EjercicioNro3 {
+
+    main()
+    float a, b,;
+    float x;
+    int l;
+
+    printf("Ingresa 2 numeros rale:");
+    scanf("%f %f", &a, &b);
+
+    printf("Ingresa la operación deseada: +-suma, --resta, *-multiplicion, /-division, %-Porcentaje, ^-potencia, R-raiz:");
+    scanf("%d", &l);
+
+    switch(l)
+    {
+    case 1:
+        {
+            x=a+b;
+            printf("%f", x);
+            break;
+        }
+    case 2:
+        {
+            x=a-b;
+            printf("%f", x);
+            break;
+        }
+    case 3:
+        {
+            x=a*b;
+            printf("%f", x);
+            break;
+        }
+        case 4:
+        {
+            x=a/b;
+            printf("%f", x);
+            break;
+        }
+        case 5:
+        {
+            x=a % b;
+            printf("%f", x);
+            break;
+        }
+        case 5:
+        {
+            x=a Potencia() b;
+            printf("%f", x);
+            break;
+        }
+        case 6:
+        {
+            x=a sqrt() b;
+            printf("%f", x);
+            break;
+        }
+    default:
+            {
+                printf("Opcion no valida");
+                break; 
+            }
+    }
+
+    system("pause");
+}
+
---
+++ ExamenWAMA/EjercicioNro5.java
@@ -0,0 +1,29 @@
+package ExamenWAMA;
+
+/**
+ * EjercicioNro5
+ */
+import java.util.Scanner;
+public class EjercicioNro5 {
+
+    Scanner lector = new Scanner(System.in);
+        float n1, n2, suma, resta, multiplicacion;
+
+        System.out.println("Ingrese el primer número:");
+        n1=lector.nextFloat();
+
+        System.out.println("Ingrese el segundo número:");
+        n2=lector.nextFloat();
+
+        suma=n1+n2;
+        resta-1n-n2;
+        multiplicacion=n1*n2;
+
+        System.out.println("La suma es "+suma);
+        System.out.println("La resta es "+resta);
+        System.out.println("La multiplicacion es "+multiplicacion);
+        
+        public static void main(String[] args) {
+            operaciones solución = new Operaciones();
+        }
+}
